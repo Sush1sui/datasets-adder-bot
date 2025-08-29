4c0c0285d677ca67abbf6f36033165ce900cc154
Sush1sui <jplovesraw@gmail.com>
2025-08-29 16:57:46 +0800

    fix: get all users

--- internal/bot/commands/getAllUsers.go
+++ internal/bot/commands/getAllUsers.go
@@ -22,18 +22,15 @@ func GetAllUsers(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	// Get all user accounts
 	userAccounts, err := repository.UserAccountService.GetAllUserAccounts()
 	if err != nil {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Flags:   discordgo.MessageFlagsEphemeral,
-				Content: "Error getting user accounts: " + err.Error(),
-			},
+		mess := "Error getting user accounts: " + err.Error()
+		s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
+			Content: &mess,
 		})
 		return
 	}
 
 	// Send the user accounts as a message
-	var content string
+	var content = "# User Accounts:\n\n"
 	for _, user := range userAccounts {
 		if user.Email != nil {
 			content += *user.Email
@@ -41,11 +38,10 @@ func GetAllUsers(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		content += "\n"
 	}
 
-	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: &discordgo.InteractionResponseData{
-			Flags:   discordgo.MessageFlagsEphemeral,
-			Content: "User accounts:\n" + content,
-		},
+	_, _ = s.ChannelMessageSend(i.ChannelID, content)
+
+	mess := "User accounts successfully fetched"
+	s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
+		Content: &mess,
 	})
 }
