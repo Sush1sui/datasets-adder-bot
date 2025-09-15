e81e806355b900d9b733ab7ff7a1c7eb1ef206d2
Sush1sui <jplovesraw@gmail.com>
2025-09-15 11:50:59 +0800

    refactor: readjusted cuz so many changes from my backend DB fields

--- internal/models/userAccountModel.go
+++ internal/models/userAccountModel.go
@@ -15,7 +15,8 @@ type Meal struct {
 }
 
 type DietHistory struct {
-    Date            time.Time         `bson:"date,omitempty" json:"date,omitempty"`
+    // date stored as "YYYY-MM-DD" string in TS
+    Date            string            `bson:"date,omitempty" json:"date,omitempty"`
     NutritionalData []NutritionalData `bson:"nutritionalData,omitempty" json:"nutritionalData,omitempty"`
     Breakfast       []Meal            `bson:"breakfast,omitempty" json:"breakfast,omitempty"`
     Lunch           []Meal            `bson:"lunch,omitempty" json:"lunch,omitempty"`
@@ -25,33 +26,45 @@ type DietHistory struct {
 
 type LoggedWeight struct {
     Value float64 `bson:"value" json:"value"`
-    Label string  `bson:"label" json:"label"`
-    Year int64   `bson:"year" json:"year"`
+    Label string  `bson:"label" json:"label"` // "Jan" | "Feb" | ...
+    Year  int     `bson:"year" json:"year"`
+}
+
+type DailyRecommendation struct {
+    Calories float64 `bson:"calories" json:"calories"`
+    Carbs    float64 `bson:"carbs" json:"carbs"`
+    Protein  float64 `bson:"protein" json:"protein"`
+    Fat      float64 `bson:"fat" json:"fat"`
 }
 
 type UserAccount struct {
-    ID                bson.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-    GmailID           *string            `bson:"gmailId,omitempty" json:"gmailId,omitempty"`
-    ProfileLink       *string            `bson:"profileLink,omitempty" json:"profileLink,omitempty"`
-    ProfilePublicID   *string            `bson:"profilePublicId,omitempty" json:"profilePublicId,omitempty"`
-    Gender            *string            `bson:"gender,omitempty" json:"gender,omitempty"`
-    BirthDate         *time.Time         `bson:"birthDate,omitempty" json:"birthDate,omitempty"`
-    HeightFeet        *float64           `bson:"heightFeet,omitempty" json:"heightFeet,omitempty"`           // in feet
-    HeightInches      *float64           `bson:"heightInches,omitempty" json:"heightInches,omitempty"`       // in inches
-    Weight            *float64           `bson:"weight,omitempty" json:"weight,omitempty"`           // in kg
-    WeightGoal        *string            `bson:"weightGoal,omitempty" json:"weightGoal,omitempty"`   // e.g., "lose", "maintain", "gain"
-    TargetWeight      *float64           `bson:"targetWeight,omitempty" json:"targetWeight,omitempty"` // in kg
-    BMI               *float64           `bson:"bmi,omitempty" json:"bmi,omitempty"`
-    Allergens         []string           `bson:"allergens,omitempty" json:"allergens,omitempty"`
-    MedicalConditions []string           `bson:"medicalConditions,omitempty" json:"medicalConditions,omitempty"`
-    DietHistory       []DietHistory      `bson:"dietHistory,omitempty" json:"dietHistory,omitempty"`
-    LoggedWeights     []LoggedWeight     `bson:"loggedWeights,omitempty" json:"loggedWeights,omitempty"`
-    Name              *string            `bson:"name,omitempty" json:"name,omitempty"`
-    FirstName         *string            `bson:"firstName,omitempty" json:"firstName,omitempty"`
-    LastName          *string            `bson:"lastName,omitempty" json:"lastName,omitempty"`
-    Email             *string            `bson:"email,omitempty" json:"email,omitempty"`
-    Password          *string            `bson:"password,omitempty" json:"password,omitempty"`
-    OTP               *string            `bson:"otp,omitempty" json:"otp,omitempty"`
-    OTPExpires        *time.Time         `bson:"otpExpires,omitempty" json:"otpExpires,omitempty"`
-    IsVerified        bool               `bson:"isVerified" json:"isVerified"`
+    ID                bson.ObjectID  `bson:"_id,omitempty" json:"id,omitempty"`
+    GmailID           *string             `bson:"gmailId,omitempty" json:"gmailId,omitempty"`
+    ProfileLink       *string             `bson:"profileLink,omitempty" json:"profileLink,omitempty"`
+    ProfilePublicID   *string             `bson:"profilePublicId,omitempty" json:"profilePublicId,omitempty"`
+    Gender            *string             `bson:"gender,omitempty" json:"gender,omitempty"`
+    BirthDate         *time.Time          `bson:"birthDate,omitempty" json:"birthDate,omitempty"`
+    HeightFeet        *float64            `bson:"heightFeet,omitempty" json:"heightFeet,omitempty"`
+    HeightInches      *float64            `bson:"heightInches,omitempty" json:"heightInches,omitempty"`
+    Weight            *float64            `bson:"weight,omitempty" json:"weight,omitempty"`
+    WeightGoal        *string             `bson:"weightGoal,omitempty" json:"weightGoal,omitempty"`
+    TargetWeight      *float64            `bson:"targetWeight,omitempty" json:"targetWeight,omitempty"`
+    BMI               *float64            `bson:"bmi,omitempty" json:"bmi,omitempty"`
+    Allergens         []string            `bson:"allergens,omitempty" json:"allergens,omitempty"`
+    MedicalConditions []string            `bson:"medicalConditions,omitempty" json:"medicalConditions,omitempty"`
+    DietHistory       []DietHistory       `bson:"dietHistory,omitempty" json:"dietHistory,omitempty"`
+    LoggedWeights     []LoggedWeight      `bson:"loggedWeights,omitempty" json:"loggedWeights,omitempty"`
+    Name              *string             `bson:"name,omitempty" json:"name,omitempty"`
+    FirstName         *string             `bson:"firstName,omitempty" json:"firstName,omitempty"`
+    LastName          *string             `bson:"lastName,omitempty" json:"lastName,omitempty"`
+    Email             *string             `bson:"email,omitempty" json:"email,omitempty"`
+    Password          *string             `bson:"password,omitempty" json:"password,omitempty"`
+    OTP               *string             `bson:"otp,omitempty" json:"otp,omitempty"`
+    OTPExpires        *time.Time          `bson:"otpExpires,omitempty" json:"otpExpires,omitempty"`
+    IsVerified        bool                `bson:"isVerified" json:"isVerified"`
+    LoginAttempts     int                 `bson:"loginAttempts,omitempty" json:"loginAttempts,omitempty"`
+    LockUntil         *time.Time          `bson:"lockUntil,omitempty" json:"lockUntil,omitempty"`
+    DietType          *string             `bson:"dietType,omitempty" json:"dietType,omitempty"`
+    DailyRecommendation *DailyRecommendation `bson:"dailyRecommendation,omitempty" json:"dailyRecommendation,omitempty"`
+    ActivityLevel     *string             `bson:"activityLevel,omitempty" json:"activityLevel,omitempty"`
 }
